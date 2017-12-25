30280238a3527381f9316ced3c579bbd3c629d90
LoggingUnit <andreserafimov@gmail.com>
2017-12-25 22:29:31 +0500

    UI functional partially implemented
    UI functional partially implemented, onload used with google api js script

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 'use strict';
 
-function initMap() {
-  var weather = new Weather('https://api.openweathermap.org/data/2.5/forecast?mode=json&units=metric&APPID=0bc7c6edc6e5bc381e503d32151b71c9');
+var buttonSubmit = document.getElementById('buttonGo');
+buttonSubmit.addEventListener('click', callback);
 
-  var googleMaps = new GoogleMaps("map", "right-panel");
-  googleMaps.initializeMap(-24.345, 134.46, 4);
+var inputFrom = document.getElementById('inputFrom');
+var inputTo = document.getElementById('inputTo');
+var inputOffset = document.getElementById('inputOffset');
+var inputStep = document.getElementById('inputStep');
 
-  let promise = new Promise(function (res, rej) {
-    googleMaps.calcRoute('Oktyabrsky', 'Piter', 1000000, res, rej);
-  });
-  promise.then(
-    response => {
-      googleMaps.setOffset(0);
-      return weather.weatherForecast(googleMaps.getRoute());
-    }
-  )
+let promiseServicesLoaded = new Promise(function (resolve, reject) {
+  var googleapiScript = document.getElementById('googleapisScript');
+  googleapiScript.onload = function () {
+    resolve('googleapisScripts loaded succesfully');
+  }
+  googleapiScript.onerror = function () {
+    reject(new Error('googleapisScripts is unable to load'));
+  }
+});
+
+promiseServicesLoaded
   .then(
-    response => {
-      console.log(weather.assignWeatherToRoute(googleMaps.getRoute(), response));
-    ;}
-  )
-} 
+  result => {
+    console.log('Done: ', result);
+    window.weather = new Weather('https://api.openweathermap.org/data/2.5/forecast?mode=json&units=metric&APPID=0bc7c6edc6e5bc381e503d32151b71c9');
+    window.googleMaps = new GoogleMaps("map", "right-panel");
+    googleMaps.initializeMap(66.788890, 93.775280, 3);
+    buttonSubmit.disabled = false;
+  },
+  error => console.log('Error: ', error.message)
+  );
+
+function callback() {
+
+
+  if (validate(inputFrom) && validate(inputTo) && validate(inputOffset) && validate(inputStep)) {
+    console.log('form validated');
+    
+    let promise1 = new Promise(function (res, rej) {
+      googleMaps.calcRoute(inputFrom.value, inputTo.value, inputStep.value, res, rej);
+    });
+    promise1.then(
+      response => {
+        googleMaps.setOffset(inputOffset.value);
+        return weather.weatherForecast(googleMaps.getRoute());
+      })
+      .then(
+      response => {
+        console.log(weather.assignWeatherToRoute(googleMaps.getRoute(), response));
+      });
+  }
+}
+
+
+
+
+
+
 
---
+++ validate.js
@@ -0,0 +1,32 @@
+function validate(arg) {
+    switch(arg.id) {
+        case 'inputFrom':
+        if (arg.value) {
+            return true;
+        }
+        break;
+
+        case 'inputTo':
+        if (arg.value) {
+            return true;
+        }
+        break;
+
+        case 'inputOffset':
+        if ((arg.value>=0)&&(arg.value<=36)) {
+            return true;
+        }
+        return false;
+        break;
+
+        case 'inputStep':
+        if (arg.value>50000) {
+            return true;
+        }
+        return false;
+        break;
+
+        default:
+        return false;
+    }
+}
