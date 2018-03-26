ad32f99fdf6aad8cb12ff672835dcadc90ced19c
LoggingUnit <andreserafimov@gmail.com>
2018-03-26 16:49:54 +0300

    Test
    Test

--- static/assets/js/UserAccount.js
+++ static/assets/js/UserAccount.js
@@ -17,9 +17,9 @@ class UserAccount {
         this.mainCalendar = new MainCalendar('#mainCalendar', myPopUpManager.popUpShow, myUIManager.uiElementSetValue);
         this.profileCalendar = new ProfileCalendar('#profileCalendar', this.eventDeleteByCalendarButtonClick.bind(this));
 
-        this.tripService = new TripService('https://weatheralongroutenodeserver.herokuapp.com/trips');
-        this.userService = new UserService('https://weatheralongroutenodeserver.herokuapp.com/users');
-        this.authService = new AuthService('https://weatheralongroutenodeserver.herokuapp.com/auth');
+        this.tripService = new TripService('https://weatheralongroute.herokuapp.com/trips');
+        this.userService = new UserService('https://weatheralongroute.herokuapp.com/users');
+        this.authService = new AuthService('https://weatheralongroute.herokuapp.com/auth');
 
         this.setItem = setItem;
         this.getItem = getItem;
